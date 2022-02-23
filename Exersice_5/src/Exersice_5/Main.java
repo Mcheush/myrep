b94c3e0f52dfcec7725366ddd7a9b0175a57f714
mcheush <mchumak1@mail.ru>
2022-02-23 23:52:28 +0500

    23.02

--- Exersice_5/src/Exersice_5/Main.java
+++ Exersice_5/src/Exersice_5/Main.java
@@ -5,8 +5,28 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	Scanner sc;
-    sc = new Scanner(System.in);
+        int a, b; // a - строки, b - столбцы, кол - во эл - тов массива
+	    Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.print("Введите количество строк матрицы ");
+        a = sc.nextInt();
+        System.out.print("Введите количство столбцов матрицы ");
+        b = sc.nextInt();
+        int [][] array = new int[a][b];
+        for (int x = 0; x < a; x++) {
+            for (int y = 0; y < b; y++) {
+                int c = (int) (Math.random() * ((100 + 100) + 1) - 100);
+                array[x][y] = c;
+            }
+        }
+        for (int[] i : array) {
+            for (int j: i) {
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+
+
 
     }
 }
