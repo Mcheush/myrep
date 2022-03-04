369537ca88d126708c65112b401688da09f08e82
mcheush <chumak-mo@teracont.ru>
2022-03-04 22:31:13 +0500

    04.03

--- Exersice_5/src/Exersice_5/Main.java
+++ Exersice_5/src/Exersice_5/Main.java
@@ -5,42 +5,54 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int a, b; // a - строки, b - столбцы, кол - во эл - тов массива
-        int d; // число, которому не должен быть равен эл - т массива
-        int e = 0;
-	    Scanner sc;
+        int xLen, lenY; // a - строки, b - столбцы, кол - во эл - тов массива
+        int inputNum; // число, которому не должен быть равен эл - т массива
+        Scanner sc;
         sc = new Scanner(System.in);
         System.out.print("Введите количество строк матрицы ");
-        a = sc.nextInt();
+        xLen = sc.nextInt();
         System.out.print("Введите количство столбцов матрицы ");
-        b = sc.nextInt();
-        int [][] array = new int[a][b];
-        for (int x = 0; x < a; x++) { // заполнение массива рандомными числами
-            for (int y = 0; y < b; y++) {
+        lenY = sc.nextInt();
+        int[][] array = new int[xLen][lenY];
+
+        for (int i = 0; i < xLen; i++) { // заполнение массива рандомными числами
+            for (int j = 0; j < lenY; j++) {
                 int c = (int) (Math.random() * ((100 + 100) + 1) - 100);
-                array[x][y] = c;
-                }
+                array[i][j] = c;
             }
+        }
+
         for (int[] i : array) {                 //Вывод матрицы в консоль
-            for (int j: i) {
-                System.out.print(j+ " ");
+            for (int j : i) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
+
         System.out.print("Введите число, которому не будет равен ни один из элеметов матрицы ");
-        d = sc.nextInt();
-        for (int[] i : array) {
-            e++;
-            for (int j: i) {
-                if (j == d) {
-                    System.out.println("Строка "+e);
-                }
+        inputNum = sc.nextInt();
+        boolean[] results = new boolean[xLen];
+        boolean res = false;
 
+        for (int i = 0; i < xLen; i++) {
+            for (int j = 0; j < lenY; j++) {
+                if (array[i][j] == inputNum) {
+                    results[i] = true;
+                    break;
+                }
             }
-            System.out.println();
         }
 
+        for (int i = 0; i < xLen; i++) {
+            if (results[i]) {
+                res = true;
+                System.out.println("Строка " +(i+1));
+            }
+        }
 
+        if (!res) {
+            System.out.println("Таких строк нет");
+        }
 
     }
 }
