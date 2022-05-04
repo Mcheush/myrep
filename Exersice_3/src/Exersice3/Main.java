dde008a1c9c9bca5ddc75682bd7682e34cfc9dba
mcheush <mchumak1@mail.ru>
2022-05-04 12:34:17 +0500

    04.05

--- Exersice_3/src/Exersice3/Main.java
+++ Exersice_3/src/Exersice3/Main.java
@@ -7,16 +7,24 @@ public class Main {
     public static void main(String[] args) {
         int x = 0, N;
         Scanner sc = new Scanner(System.in);
-        ArrayList Array = new ArrayList();
+        ArrayList <Integer> Array = new ArrayList(); //Создаю динамический массив
         do {
             System.out.print("Введите N ");
             N = sc.nextInt();
-            Array.add(x, N);
-            x++;
+            if (N != 0)  //Проверяю перв. введен. число, если 0 то он будет max если все числа отрицательные
+                Array.add(x, N); // Заполняю динамический массив
+                x++;
+
         }
-        while (N != 0);
-        System.out.print("Максимальное из введенных чисел: "+ Collections.max(Array));
+        while (N != 0); //Цикл выполняется пока не будет введен ноль
 
+        int y = Array.size(); // Кладу в переменную значение величины получившегося массива
 
+        if (y == 0) { // Если в массиве нет чисел, то ничего не введено
+            System.out.print("Чисел нет");
+        }
+        else {
+            System.out.print("Наибольшее число " + Collections.max(Array)); // Вывожу max число
+        }
     }
 }
--- Exersice_5/src/Exersice_5/Main.java
+++ Exersice_5/src/Exersice_5/Main.java
@@ -19,22 +19,17 @@ public class Main {
             for (int j = 0; j < lenY; j++) {
                 int c = (int) (Math.random() * ((100 + 100) + 1) - 100);
                 array[i][j] = c;
-            }
-        }
-
-        for (int[] i : array) {                 //Вывод матрицы в консоль
-            for (int j : i) {
-                System.out.print(j + " ");
+                System.out.print(c + " "); //Вывод матрицы в консоль
             }
             System.out.println();
         }
 
         System.out.print("Введите число, которому не будет равен ни один из элеметов матрицы ");
         inputNum = sc.nextInt();
-        boolean[] results = new boolean[xLen];
+        boolean[] results = new boolean[xLen]; //Создаю массив для поиска числа в массиве
         int stat = 0;
 
-        for (int i = 0; i < xLen; i++) {
+        for (int i = 0; i < xLen; i++) { // Если в массиве array есть введенное число, то эл-т results = true
             for (int j = 0; j < lenY; j++) {
                 if (array[i][j] == inputNum) {
                     results[i] = true;
@@ -43,13 +38,15 @@ public class Main {
             }
         }
 
-        for (int i = 0; i < xLen; i++) {
-            if (results[i]) {
+        int temp; //Счетчик для проверки на то, все ли эл-ты results[] == true
+
+        for (temp = 0; temp < xLen; temp++) { //Цикл для счетсика, каждый раз когда InputNum==true, то stat++
+            if (results[temp]) {
                 stat++;
             }
         }
 
-        if (stat != 0) {
+        if (stat != temp) { //Здесь проверка на то, все ли эл-ты results[] == true
             for (int i = 0; i < xLen; i++) {
                 if (!results[i]) {
                     System.out.println("Строка " +(i+1));
